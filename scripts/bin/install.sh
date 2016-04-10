e3848771c26ee81d74e07e7575a97586a3bafa1c
Fred Zimmerman <fred@pagekicker.com>
2016-04-10 16:41:03 -0400

    Renamed files for clarity Removed hardcoded paths Updated configuration template file

---
+++ scripts/bin/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# installs bitnami magento stack without plugins
+
+echo "installing bitnami downloadable $1"
+./$1 # first parameter is path to bitnami stack download, this runs it
+
+# creates additional plugin directories that are needed for a full pagekicker store installation to operate
+# book builder and create a catalog entry scripts expect these directories as destinations for file builds
+
+. ../conf/config.txt
+
+mkdir -m 755 $SFB_MAGENTO_HOME"var/import" 
+mkdir -m 755 $SFB_MAGENTO_HOME"var/export"
+mkdir -m 755 $SFB_MAGENTO_HOME"media/webforms"
+mkdir -m 755 $SFB_MAGENTO_HOME"media/webforms/xml"
