848140a70e716a6120c73f64c3cce4ce2985dd90
Fred Zimmerman <fred@pagekicker.com>
2016-05-01 14:42:08 -0400

    test scripts no longer throw out stdout

--- test/revolutionary-figures.sh
+++ test/revolutionary-figures.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bin/create-catalog-entry.sh --builder "yes" --booktitle "Test" --yourname "Fred" --jobprofilename "Default" --import "no" --passuuid "$uuid" --seedfile "seeds/revolutionary_figures" 1> /dev/null
+bin/create-catalog-entry.sh --builder "yes" --booktitle "Test" --yourname "Fred" --jobprofilename "Default" --import "no" --passuuid "$uuid" --seedfile "seeds/revolutionary_figures" 
