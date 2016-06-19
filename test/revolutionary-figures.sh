4b2f9513fb7fe4a5062f0f15882460feaa21efe3
Fred Zimmerman <fred@pagekicker.com>
2016-06-18 21:39:18 -0400

    makes test script exit gracefully on missing config file

--- test/revolutionary-figures.sh
+++ test/revolutionary-figures.sh
@@ -3,6 +3,8 @@
 if [ ! -f "$HOME"/.pagekicker/config.txt ]; then
 	echo "config file not found, creating /home/<user>/.pagekicker, put config file there"
 	mkdir -p -m 755 "$HOME"/.pagekicker
+	echo "exiting"
+	exit 1
 else
 	. "$HOME"/.pagekicker/config.txt
 	echo "read config file from $HOME""/.pagekicker/config.txt"
