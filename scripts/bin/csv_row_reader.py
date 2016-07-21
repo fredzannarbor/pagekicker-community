2dfa4d04a16dac25f1095bda6cb5a3a66b5bbdc1
Fred Zimmerman <fred@pagekicker.com>
2016-07-21 14:35:00 -0400

    Initial commit of csv_row_reader.py

---
+++ scripts/bin/csv_row_reader.py
@@ -0,0 +1,47 @@
+# reads a single row of csv 
+# code by @martineau 
+# http://stackoverflow.com/questions/38489761/how-can-i-select-only-a-particular-row-in-a-csv-file
+
+import csv
+from contextlib import contextmanager
+import sys
+import itertools
+import os
+
+@contextmanager
+def multi_file_manager(files, mode='w'):
+    """ Context manager for multiple files. """
+    files = [open(file, mode) for file in files]
+    yield files
+    for file in files:
+        file.close()
+
+def csv_read_row(filename, n):
+    """ Read and return nth row of a csv file, counting from 1. """
+    with open(filename, 'rb') as f:
+        reader = csv.reader(f)
+        return next(itertools.islice(reader, n-1, n))
+
+if len(sys.argv) != 4:
+    print('usage: utility <csv filename> <uuid> <target row>')
+    sys.exit(1)
+
+tmpdir = "/tmp/pagekicker"
+f = sys.argv[1]
+uuid = sys.argv[2]
+target_row = int(sys.argv[3])
+folder = os.path.join(tmpdir, uuid)
+print folder
+
+destinations= [folder+dest for dest in ('/csv/row.editedby', 
+'/csv/row.booktitle', 
+'/csv/row.seeds', 
+'/csv/row.imprint')]
+
+print destinations
+
+with multi_file_manager(destinations, mode='w') as files:
+    row = csv_read_row(f, target_row)
+    #editedby, booktitle, seeds, imprint = row[0], row[2], row[3], row[4]
+    for i,j in zip(range(4), (0, 2, 3, 4)):
+        files[i].write(row[j]+'\n')
