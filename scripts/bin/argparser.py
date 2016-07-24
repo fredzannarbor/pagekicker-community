35039b878e280065b637dd4b1b4764925ed63021
Fred Zimmerman <fred@pagekicker.com>
2016-07-24 13:02:48 -0400

    adds add_this_content & _part_name flags to argparser.py

--- scripts/bin/argparser.py
+++ scripts/bin/argparser.py
@@ -13,7 +13,10 @@ parser.add_argument("--lang", help="wiki language bigram", default = 'en')
 parser.add_argument("--request_type", help="request type", default = 'sum')
 parser.add_argument("--outfile", help = "path to outfile", default = 'outfile')
 parser.add_argument("--summary", help = "true or false", action = "store_true")
-parser.add_argument("--pagehits", help = "path to list of page hits", default = 'pagehits')
+parser.add_argument("--add_this_content", help = "path to document filename", default = 'none')
+parser.add_argument("--add_this_content_part_name", help = "part heading in ToC", action = "User-Added Content")
+
+
 args = parser.parse_args()
 
 input_file = args.infile
