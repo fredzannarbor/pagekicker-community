0cca9784a55ad0f940f20f8e341fcb07e6ca200a
Fred @ PageKicker <fred@PageKicker.com>
2016-08-03 23:04:55 +0000

    Adds tests for PRS scenarios, PRS imprint

--- test/dat-bitnami-test.sh
+++ test/dat-bitnami-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # nonportable test script for bitnami
-bin/xform.sh /opt/bitnami/apps/magento/htdocs/media/webforms/xml 3404.xml
+bin/xform.sh /opt/bitnami/apps/magento/htdocs/media/webforms/xml mdtest.xml
 # next job tests exit on insufficient words
 # bin/xform.sh /opt/bitnami/apps/magento/htdocs/media/webforms/xml 3401.xml
 
