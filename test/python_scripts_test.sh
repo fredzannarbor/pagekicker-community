028fd1368d57b24574a53aa331a3678f1efedf92
Fred Zimmerman <fred@pagekicker.com>
2016-08-23 14:12:27 -0400

    adds test script for summaries only, tests to python_scripts_test

--- test/python_scripts_test.sh
+++ test/python_scripts_test.sh
@@ -1,7 +1,12 @@
 # python script tests
 
+
+
 python3 scripts/bin/wikifetcher.py --infile ../seeds/paella --outfile ../scr/test
 python2.7 bin/PKsum.py -l "5" -o scr/sumtest ../test/tmpbody.md 
 python3 bin/PKsum.py -l "5" -o scr/sumtest ../test/tmpbody.md #not working right now
 python2.7 bin/nerv3.py ../test/tmpbody.md scr/nervoutput 123
+python bin/csvreader.py "../test/1001_1.csv" "123" 1
+python3.5 bin/csvreader.py "../test/1001_1.csv" "123" 1
+python2.7 bin/csvreader.py "../test/1001_1.csv" "123" 1
 
