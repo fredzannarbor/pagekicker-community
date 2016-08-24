b7d341d33b3938829b2872490b183593a25f0c25
Fred Zimmerman <fred@pagekicker.com>
2016-08-24 02:10:11 -0400

    fixes module name

--- scripts/bin/ubuntu-installer.sh
+++ scripts/bin/ubuntu-installer.sh
@@ -4,7 +4,7 @@
 
 sudo apt-get install xmlstarlet default-jre pdftk fdupes pdfimages imagemagick sendemail pandoc python-numpy python-sklearn python-networkx python-nltk #python libraries for python 2.7
 
-pip3 install scikit-learn networkx AlchemyAPI nltk time wikipedia scipy numpy 
+pip3 install scikit-learn networkx alchemyapi nltk time wikipedia scipy numpy 
 
 # font management
 
