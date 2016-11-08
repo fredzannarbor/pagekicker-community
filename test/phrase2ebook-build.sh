13f89a5a1cc40b96e09ecb1f2de5db4e57d2c30a
Fred Zimmerman <fred@pagekicker.com>
2016-11-08 16:36:53 -0500

    adds phrase2ebook-build required to work with 21 app Book Builder
        * fixes annoying creation of extra folder in /tmp/pagekicker
        * adds **** BOOK BUILDER **** running head
        * returns epub to $TMPDIR/delivery.epub
        * in future, fix Book Builder to return $TMPDIR$UUID/delivery.epub

---
+++ test/phrase2ebook-build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "**** BOOK BUILDER ***"
+if [ ! -f "$HOME"/.pagekicker/config.txt ]; then
+	echo "config file not found, creating /home/<user>/.pagekicker, put config file there"
+	mkdir -p -m 755 "$HOME"/.pagekicker
+	echo "exiting"
+	exit 1
+else
+	. "$HOME"/.pagekicker/config.txt
+	echo "read config file from $HOME""/.pagekicker/config.txt"
+fi
+
+
+. includes/set-variables.sh
+
+if [ ! "$passuuid" ] ; then
+	#echo "creating uuid"
+	uuid=$("$PYTHON_BIN"  -c 'import uuid; print(uuid.uuid1())')
+	echo "uuid is" $uuid
+	mkdir -p -m 777 $TMPDIR$uuid
+else
+	uuid=$passuuid
+	echo "received uuid " $uuid
+	mkdir -p -m 777 $TMPDIR$uuid
+fi
+
+
+$scriptpath"bin/builder.sh" --singleseed "$1" --booktitle "$2" --jobprofilename "$3" --passuuid "$uuid"
+cp $TMPDIR$uuid/*.epub $TMPDIR"/delivery.epub"
+
+if [ ! -f "$TMPDIR$uuid/ebookcover.jpg" ]; then
+    echo "error: cover not found! at "$TMPDIR$uuid"/ebookcover.jpg"  > "$TMPDIR$uuid/error.log"
+fi
+exit 0
+# if error log is empty then PASS
+echo "\n"
+echo "PASS" "  $uuid"  | tee -a $LOCAL_DATA/logs/error.log
