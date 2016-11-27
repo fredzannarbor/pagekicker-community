f32d8c04a2adda5e6c835a5e4a2b8570bf42a6d1
Fred Zimmerman <fred@pagekicker.com>
2016-11-26 19:56:55 -0500

    tweaks skyscraper presentation

--- scripts/bin/builder.sh
+++ scripts/bin/builder.sh
@@ -1005,7 +1005,7 @@ if [ -z "$skyscraper" ]; then
 	echo "no skyscraper"
 else
 
-	sed -i '1i # Unique Proper Nouns and Key Terms'  "$TMPDIR"$uuid/all_nouns.txt
+	sed -i '1i # Important Proper Nouns and Key Terms'  "$TMPDIR"$uuid/all_nouns.txt
 	sed -i '1i \'  "$TMPDIR"$uuid/all_nouns.txt
 	sed -i G  "$TMPDIR"$uuid/all_nouns.txt
 	echo '\pagenumbering{gobble}' > $TMPDIR$uuid/all_nouns_sky.txt
@@ -1013,13 +1013,16 @@ else
 	sed -n 1,25p $TMPDIR$uuid/all_nouns.txt >> $TMPDIR$uuid/all_nouns_sky.txt
 	cp  "$TMPDIR"$uuid/all_nouns.txt  "$TMPDIR"$uuid/all_nouns.md
 	cp  "$TMPDIR"$uuid/all_nouns_sky.txt  "$TMPDIR"$uuid/all_nouns_sky.md
+ # make pdfs
 	pandoc $TMPDIR$uuid/all_nouns_sky.md --latex-engine=xelatex --template=$confdir"pandoc_templates/nonumtemplate.tex" -o $TMPDIR$uuid/all_nouns_sky.pdf
 	pandoc $TMPDIR$uuid/pp_summary_sky.md --latex-engine=xelatex --template=$confdir"pandoc_templates/nonumtemplate.tex" -o $TMPDIR$uuid/pp_summary_sky.pdf
+# make pngs
 	convert -density 400 $TMPDIR$uuid/pp_summary_sky.pdf -trim $TMPDIR$uuid/pp_summary_sky.png
 	convert $TMPDIR$uuid/pp_summary_sky.png -border 30 $TMPDIR$uuid/pp_summary_sky.png
 	convert -density 400 $TMPDIR$uuid/all_nouns_sky.pdf -trim $TMPDIR$uuid/all_nouns_sky.png
 	convert $TMPDIR$uuid/all_nouns_sky.png -border 30 $TMPDIR$uuid/all_nouns_sky.png
-	montage $TMPDIR$uuid"/pp_summary_sky.png" $TMPDIR$uuid"/cover/wordcloudcover.png" $TMPDIR$uuid"/all_nouns_sky.png"  -geometry 1000x2000 -tile 1x10 -mode concatenate $TMPDIR$uuid/skyscraper.png
+# make skyscraper
+	montage $TMPDIR$uuid"/all_nouns_sky.png"  $TMPDIR$uuid"/cover/wordcloudcover.png" $TMPDIR$uuid"/pp_summary_sky.png" -geometry 1000x3000 -tile 1x10 -mode concatenate $TMPDIR$uuid/skyscraper.png
 	convert $TMPDIR$uuid"/skyscraper.png" -trim -border 30 $TMPDIR$uuid/skyscraper.png
 	echo "built skyscraper"
 fi
