a296ea687c5862c2f61f0ec77c3f982a22dfd62d
Fred Zimmerman <fred@pagekicker.com>
2016-12-06 15:11:49 -0500

    initial commit of version1.0 tester

---
+++ test/version1.0_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# version1.0 test suite
+# runs test of all production-visible scripts
+
+echo "------------------------version 1.0-------------------------------"
+
+if shopt -q  login_shell ; then
+
+	if [ ! -f "$HOME"/.pagekicker/config.txt ]; then
+		echo "config file not found, creating /home/<user>/.pagekicker, put config file there"
+		mkdir -p -m 755 "$HOME"/.pagekicker
+		echo "exiting"
+		exit 1
+	else
+		. "$HOME"/.pagekicker/config.txt
+		echo "read config file from login shell $HOME""/.pagekicker/config.txt"
+	fi
+else
+	. /home/$(whoami)/.pagekicker/config.txt #hard-coding /home is a hack
+	echo "read config file from nonlogin shell /home/$(whoami)/.pagekicker/config.txt"
+fi
+
+cd $scriptpath
+
+. includes/set-variables.sh
+
+. ../test/paella.sh
