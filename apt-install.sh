044617202dd1b1368b1eb60a153f328496777e11
Fred Zimmerman <fred@pagekicker.com>
2017-02-08 15:04:30 -0500

    isolates install functions for easier testing
    * apt-install is all sudo apt-get install commands

---
+++ apt-install.sh
@@ -0,0 +1,20 @@
+sudo apt-get install -y \
+apache2 \
+build-essential \
+default-jre \
+fdupes \
+git \
+imagemagick \
+mysql-client \
+mysql-server \
+pandoc \
+pdftk \
+perl \
+php \
+poppler-utils \
+python2.7 \
+python3-dev \
+python-pip \
+python3-pip \
+sendemail \
+xmlstarlet
