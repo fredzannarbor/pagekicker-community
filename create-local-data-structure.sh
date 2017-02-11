6a83dcf3a1c6652cba89c3c83b606f3280d5a4bb
Fred Zimmerman <fred@pagekicker.com>
2017-02-11 14:48:52 -0500

    isolates create-local-data-structure for ease of testing

---
+++ create-local-data-structure.sh
@@ -0,0 +1,8 @@
+mkdir -p local-data/bibliography local-data/bibliography/imprints local-data/bibliography/imprints/pagekicker
+mkdir -p local-data/bibliography/robots local-data/bibliography/robots/default
+mkdir -p local-data/bibliography/yaml
+mkdir -p local-data/jobprofile_builds/default
+mkdir -p local-data/logs/uuids
+mkdir -p local-data/seeds/history
+echo "1000001" > local-data/SKUs/sku_list
+touch local-data/bibliography/robots/default/default_titles.txt
--- simple-install.sh
+++ simple-install.sh
@@ -31,14 +31,7 @@ pip3 install -r requirements.txt
 
 # create local-data hierarchy
 
-mkdir -p local-data/bibliography local-data/bibliography/imprints local-data/bibliography/imprints/pagekicker
-mkdir -p local-data/bibliography/robots local-data/bibliography/robots/default
-mkdir -p local-data/bibliography/yaml
-mkdir -p local-data/jobprofile_builds/default
-mkdir -p local-data/logs/uuids
-mkdir -p local-data/seeds/history
-echo "1000001" > local-data/SKUs/sku_list
-touch local-data/bibliography/robots/default/default_titles.txt
+. create-local-data-structure.sh
 
 # get lib programs
 
