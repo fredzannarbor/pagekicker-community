33f614570d146c985bccba73588bd8aec3cf39bc
Fred Zimmerman <fred@pagekicker.com>
2017-02-11 23:19:16 -0500

    incorporates all includes into single simple-install.sh

--- simple-install.sh
+++ simple-install.sh
@@ -2,6 +2,15 @@
 
 # This is the simple version of the install script for PageKicker.
 #
+# get master repository via git clone
+
+#cd ~
+#git clone https://github.com/fredzannarbor/pagekicker-community.git
+
+# cd into the repo and run this script
+# cd pagekicker-community
+# ./simple-install.sh
+# it will do the following
 
 # create outside repo directory structure
 
@@ -9,19 +18,37 @@ mkdir -m 755 ~/.pagekicker
 mkdir -m 777 -p  /tmp/pagekicker
 mkdir -m 755 ~/magento  # stub directory for optional import/export to catalog
 
-# get master repository
-
-#cd ~
-#git clone https://github.com/fredzannarbor/pagekicker-community.git
-
 # put default configuration file in place
 # inspect it to make sure paths are correct
 
 cp ~/pagekicker-community/conf/config_defaults.txt ~/.pagekicker/config.txt
 
 # installs ubuntu dependencies
+# takes 20-30 min on 12 MB download
 
-./apt-install.sh
+sudo apt-get install -y \
+apache2 \
+build-essential \
+calibre \
+default-jre \
+fdupes \
+git \
+imagemagick \
+mysql-client \
+pandoc \
+pdftk \
+perl \
+php \
+poppler-utils \
+python2.7 \
+python3-dev \
+python-pip \
+python3-pip \
+sendemail \
+texlive-xetex \
+ttf-dejavu \
+xmlstarlet
+
 
 # install python dependencies
 
@@ -31,12 +58,31 @@ pip3 install -r requirements.txt
 
 # create local-data hierarchy
 
-. create-local-data-structure.sh
+mkdir -p local-data/bibliography local-data/bibliography/imprints local-data/bibliography/imprints/pagekicker
+mkdir -p local-data/bibliography/robots local-data/bibliography/robots/default
+mkdir -p local-data/bibliography/yaml
+mkdir -p local-data/jobprofile_builds/default
+mkdir -p local-data/logs/uuids
+mkdir -p local-data/seeds/history
+mkdir -p local-data/seeds/SKUs
+echo "1000001" > local-data/SKUs/sku_list
+touch local-data/bibliography/robots/default/default_titles.txt
 
-# get lib programs
 
-./get-lib.sh # fetches third party apps stored in PageKicker scripts/lib
+# fetches & deploys third party apps stored in PageKicker scripts/lib
+
+cd ~/pagekicker-community/scripts/lib
+git clone https://github.com/jarun/googler.git
+mkdir KindleGen
+cd KindleGen
+wget http://kindlegen.s3.amazonaws.com/kindlegen_linux_2.6_i386_v2_9.tar.gz
+tar -xvf kindlegen_linux_2.6_i386_v2_9.tar.gz
 
 # set up imagemagick configuration
 
-./magick-setup.sh
+mkdir ~/.magick
+cd ~/pagekicker-community
+cp conf/colors.xml ~/.magick/colors.xml
+conf/imagemagick-fonts.pl > ~/.magick/fonts.xml
+
+echo "install script finished running"
