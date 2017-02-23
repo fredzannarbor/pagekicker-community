7ed806414ffcd48b22d6a1a4630fd3701e7d2118
Fred Zimmerman <fred@pagekicker.com>
2017-02-23 14:40:41 -0500

    makes tldr_auto 72 characters instead of 60

--- scripts/includes/tldr_auto.sh
+++ scripts/includes/tldr_auto.sh
@@ -5,8 +5,10 @@ if [ -z "$tldr" ] ; then
   echo "  " > $TMPDIR$uuid/tldr.md
   echo "  " >> $TMPDIR$uuid/tldr.md
   echo "# TL;DR: ""$tldr" >> $TMPDIR$uuid/tldr.md
-	sed -n 1p $TMPDIR$uuid/pp_summary_all.txt | cut -c 1-60 >> $TMPDIR$uuid/tldr.txt
-  sed -n 1p $TMPDIR$uuid/pp_summary_all.txt | cut -c 1-60 >> $TMPDIR$uuid/tldr.md
+	sed -n 1p $TMPDIR$uuid/pp_summary_all.txt | cut -c 1-72 >> $TMPDIR$uuid/tldr.txt
+  sed -n 1p $TMPDIR$uuid/pp_summary_all.txt | cut -c 1-72 >> $TMPDIR$uuid/tldr.md
+	# 60 was not enough, 80 would be a full line, 72 is conventional
+	echo "automatically generated tldr is $TMPDIR$uuid/tldr.txt"
   echo "..." >> $TMPDIR$uuid/tldr.txt
   echo "..." >> $TMPDIR$uuid/tldr.md
   echo "  " >> $TMPDIR$uuid/tldr.md
