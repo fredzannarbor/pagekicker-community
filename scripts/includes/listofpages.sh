c27c6ad7d44d675cd1e34a4d27604ec49217d3ba
Fred Zimmerman <fred@pagekicker.com>
2017-03-01 01:22:58 -0500

    fixes regression with ".md" file in $TMPDIR

--- scripts/includes/listofpages.sh
+++ scripts/includes/listofpages.sh
@@ -1,7 +1,7 @@
-echo '\pagenumbering{gobble}' > $TMPDIR$uuid/listofpages.md
-echo "  " >>  "$TMPDIR"$uuid/listofpages.md
-echo "# Chapters" >>  "$TMPDIR"$uuid/listofpages.md
-echo "  " >>  "$TMPDIR"$uuid/listofpages.md
-cat "$TMPDIR"$uuid/seeds/filtered.pagehits | sed G >>  "$TMPDIR"$uuid/listofpages.md
-echo "  " >>  "$TMPDIR"$uuid/listofpages.md
-echo "  " >>  "$TMPDIR"$uuid/listofpages.md
+echo '\pagenumbering{gobble}' > "$TMPDIR$uuid/listofpages.md"
+echo "  " >>  "$TMPDIR$uuid/listofpages.md"
+echo "# Chapters" >>  "$TMPDIR$uuid/listofpages.md"
+echo "  " >>  "$TMPDIR$uuid/listofpages.md"
+cat "$TMPDIR$uuid/seeds/filtered.pagehits" | sed G >> "$TMPDIR$uuid/listofpages.md"
+echo "  " >>  "$TMPDIR$uuid/listofpages.md"
+echo "  " >>  "$TMPDIR$uuid/listofpages.md"
