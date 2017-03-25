de61dc8d0defa1949d7da748a6bb4389048a7cec
Fred Zimmerman <fred@pagekicker.com>
2017-03-25 16:01:29 -0400

    fixes regression with missing version number in Changelog section

--- scripts/includes/changelog.sh
+++ scripts/includes/changelog.sh
@@ -6,7 +6,7 @@ echo "  " >>  "$TMPDIR$uuid/changelog.md"
 echo "A key advantage of the PageKicker system is that the software is always improving. This is the way we keep track of the feature enhancements and bug fixes. For more info, see the open source repository at "$COMMUNITY_GITHUB_REPO "." >> "$TMPDIR"$uuid/changelog.md
 echo "  " >>  "$TMPDIR$uuid/changelog.md"
 echo "## Version"   >>  "$TMPDIR$uuid/changelog.md"
-echo "Version number is " $SFB_VERSION"." "$TMPDIR$uuid/changelog.md"
+echo "Version number is " $SFB_VERSION"." >> "$TMPDIR$uuid/changelog.md"
 echo "  " >>  "$TMPDIR$uuid/changelog.md"
 echo "## Commits"   >>  "$TMPDIR$uuid/changelog.md"
 echo "  " >>  "$TMPDIR$uuid/changelog.md"
