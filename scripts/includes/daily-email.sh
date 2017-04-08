9c0a403316e27c271c1012d25de9e98692bc2ae4
Fred Zimmerman <fred@pagekicker.com>
2017-04-07 20:15:14 -0400

    initial commit of daily-email booktype

---
+++ scripts/includes/daily-email.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# --booktype="daily-email"
+# Front and back matter are minimized, candidate sentences for executive
+# summary are provided."
+
+# 1. create any unique parts of the book  that are needed
+# 2. concatenates them and deliver complete.md to builder.
+
+# tweak pp_summary to create Executive_summary
+
+
+summary_length="5"
+
+echo "creating daily-email"
+mkdir -m 775 -p "$TMPDIR$uuid/daily-email"
+
+N=1
+while read -r line
+do
+    sed -n "$N"p "$TMPDIR$uuid/seeds/filtered.pagehits" > "$TMPDIR$uuid/daily-email/thisfile$N"
+   "$PYTHON_BIN"  $scriptpath"bin/wikifetcher.py" \
+  --infile "$TMPDIR$uuid/daily-email/thisfile$N" \
+  --outfile "$TMPDIR$uuid/daily-email/wiki$N.md" \
+  --lang "$wikilocale" \
+  --summary
+  cat "$TMPDIR$uuid/daily-email/wiki$N.md" >> "$TMPDIR$uuid/daily-email/postpend.md"
+  ((N++))
+done < "$TMPDIR$uuid/seeds/filtered.pagehits"
+
+echo "# PageKicker Daily" >> "$TMPDIR$uuid/daily-email/daily-email.md"
+echo "  " >> "$TMPDIR$uuid/daily-email/daily-email.md"
+cat "$TMPDIR$uuid/daily-email/postpend.md" >> "$TMPDIR$uuid/daily-email/daily-email.md"
+pandoc -o "$TMPDIR$uuid/daily-email/daily-email.html" "$TMPDIR$uuid/daily-email/daily-email.md"
+#emailbody=$(<$TMPDIR$uuid/daily-email.md)
+sendemail -t "wfzimmerman@gmail.com" \
+  -u "PageKicker Daily" \
+  -f "$GMAIL_ID" \
+  -cc "$GMAIL_ID" \
+  -xu "$GMAIL_ID" \
+  -xp "$GMAIL_PASSWORD" \
+  -s smtp.gmail.com:587 \
+  -v \
+  -o tls=yes \
+  -o message-content-type=html \
+   -o message-file="$TMPDIR$uuid/daily-email/daily-email.html"
