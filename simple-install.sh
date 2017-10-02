cb54f47d5409160b3ef9b1482a9ed9705d880a27
Fred Zimmerman <wfzimmerman@gmail.com>
2017-10-02 15:31:12 -0400

    adds stanford-ner

--- simple-install.sh
+++ simple-install.sh
@@ -15,7 +15,7 @@
 # create outside repo directory structure
 
 mkdir -m 755 ~/.pagekicker
-mkdir -m 777 -p /tmp/pagekicker
+mkdir -m 777 -p  /tmp/pagekicker
 mkdir -m 755 ~/magento  # stub directory for optional import/export to catalog
 
 # put default configuration file in place
@@ -24,50 +24,50 @@ mkdir -m 755 ~/magento  # stub directory for optional import/export to catalog
 
 cp ~/pagekicker-community/conf/config_defaults.txt "$HOME"/.pagekicker/config.txt
 
-# # installs mac dependencies
-#
-# install_dependencies_mac(){
-#     print_step "Installing 21 and its dependencies on your Mac..."
-#     if ! program_exists "brew"; then
-#         # from brew.sh
-#         print_step "Installing brew..."
-#         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#     fi
-#
-#     if ! program_exists "python3"; then
-#         print_and_run "brew install python3"
-#     fi
-#
-# # current Mac default is 3.3.6.1 - needs 3.4 and above IIRC
-#
-#     if ! program_exists "pip3"; then
-#         print_step "Installing Pip3 package manager..."
-#         curl -O https://bootstrap.pypa.io/get-pip.py
-#         sudo python3 get-pip.py
-#         rm get-pip.py
-#     fi
-#
-# }
-#
-# brew install calibre
-# brew install fdupes
-# brew install imagemagick
-# brew cask install java
-# brew install mysql-client
-# brew install pandoc
-# brew install pdfgrep
-# brew install pdftk
-# brew install poppler
-# brew install sendemail
-# brew cask install basictex
-# # brew install of pdftk is problematic
-# # see https://gist.github.com/jvenator/9672772a631c117da151
-# brew install https://raw.githubusercontent.com/turforlag/homebrew-cervezas/master/pdftk.rb
-# brew install wget # needed for Mac for install - replace with curl?
-# brew install xmlstarlet
-#
-# brew tap caskroom/fonts
-# brew cask install font-dejavu-sans
+# installs mac dependencies
+
+install_dependencies_mac(){
+    print_step "Installing 21 and its dependencies on your Mac..."
+    if ! program_exists "brew"; then
+        # from brew.sh
+        print_step "Installing brew..."
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
+    if ! program_exists "python3"; then
+        print_and_run "brew install python3"
+    fi
+
+# current Mac default is 3.3.6.1 - needs 3.4 and above IIRC
+
+    if ! program_exists "pip3"; then
+        print_step "Installing Pip3 package manager..."
+        curl -O https://bootstrap.pypa.io/get-pip.py
+        sudo python3 get-pip.py
+        rm get-pip.py
+    fi
+
+}
+
+brew install calibre
+brew install fdupes
+brew install imagemagick
+brew cask install java
+brew install mysql-client
+brew install pandoc
+brew install pdfgrep
+brew install pdftk
+brew install poppler
+brew install sendemail
+brew cask install basictex
+# brew install of pdftk is problematic
+# see https://gist.github.com/jvenator/9672772a631c117da151
+brew install https://raw.githubusercontent.com/turforlag/homebrew-cervezas/master/pdftk.rb
+brew install wget # needed for Mac for install - replace with curl?
+brew install xmlstarlet
+
+brew tap caskroom/fonts
+brew cask install font-dejavu-sans
 
 # installs ubuntu dependencies
 # takes 20-30 min on 12 MB download
@@ -125,6 +125,8 @@ mkdir KindleGen
 cd KindleGen
 wget http://kindlegen.s3.amazonaws.com/kindlegen_linux_2.6_i386_v2_9.tar.gz
 tar -xvf kindlegen_linux_2.6_i386_v2_9.tar.gz
+wget https://nlp.stanford.edu/software/stanford-ner-2017-06-09.zip
+unzip stanford-ner-2017-06-09.zip
 
 # fix hard-coded IBMcloud configuration file
 
