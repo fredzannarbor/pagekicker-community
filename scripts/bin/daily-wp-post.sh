621806785fb23599a2e80c76d4f2963c4398919d
Fred Zimmerman <wfzimmerman@gmail.com>
2018-05-30 20:24:21 -0400

    adds daily bot post

--- scripts/bin/daily-wp-post.sh
+++ scripts/bin/daily-wp-post.sh
@@ -20,6 +20,10 @@ cd $scriptpath
 
 . includes/set-variables.sh
 
+
+my_twitter_handle="fredzannarbor"
+name="Samuel Johnson"
+postauthorid="2"
 uuid=$("$PYTHON_BIN"  -c 'import uuid; print(uuid.uuid1())')
 
 echo "creating daily wordpress post"
@@ -28,14 +32,37 @@ echo "$TMPDIR$uuid"
 mkdir -m 775 -p "$TMPDIR$uuid/wordpress"
 
 
-echo "$(fortune johnson)" >> "$TMPDIR$uuid/wordpress/johnson_wotd.txt"
+$FORTUNE_BIN johnson >> "$TMPDIR$uuid/wordpress/johnson_wotd.txt"
 cat "$TMPDIR$uuid/wordpress/johnson_wotd.txt"
 wotd_name=$(cat "$TMPDIR$uuid/wordpress/johnson_wotd.txt" | head -n 1 | cut -d" " -f1)
-echo "$wotd_name"
+if [ "$wotd_name" = "To" ] ;
+  then
+		wotd_name=$(cat "$TMPDIR$uuid/wordpress/johnson_wotd.txt" | head -n 1 | cut -d" " -f2)
+	else
+		true
+fi
+# echo "$wotd_name"
 # echo "$wotd_name" > "$TMPDIR$uuid/wordpress/johnson_wotd.md"
 
-# cat "$TMPDIR$uuid/wordpress/johnson_wotd.txt" >>  "$TMPDIR$uuid/wordpress/johnson_wotd.md"
+echo "## My Word of the Day" >> "$TMPDIR$uuid/wordpress/wotd_h2.txt"
 
-pandoc -s -o "$TMPDIR$uuid/wordpress/johnson_wotd.html" "$TMPDIR$uuid/wordpress/johnson_wotd.txt"
+cat "$TMPDIR$uuid/wordpress/wotd_h2.txt" "$TMPDIR$uuid/wordpress/johnson_wotd.txt" >>  "$TMPDIR$uuid/wordpress/johnson_wotd.md"
 
-"$WP_BIN" post create "$TMPDIR$uuid/wordpress/johnson_wotd.html"  --post_type=post --post_status="publish" --post_title="Samuel Johnson WOTD $wotd_name" --post_mime_type=html --post_category="words-language"
+
+echo -e "\n" >> "$TMPDIR$uuid/wordpress/johnson_wotd.md"
+
+
+echo "## Recent references to $wotd_name on Twitter" >> "$TMPDIR$uuid/wordpress/johnson_wotd.md"
+
+t search all "$wotd_name lang:en -RT -$my_twitter_handle" >> "$TMPDIR$uuid/wordpress/johnson_wotd.md"
+
+echo -e "\n" >> "$TMPDIR$uuid/wordpress/johnson_wotd.md"
+
+
+echo "## Recent references to $name on Twitter" >> "$TMPDIR$uuid/wordpress/johnson_wotd.md"
+
+t search all "Samuel Johnson lang:en -RT -$my_twitter_handle" >> "$TMPDIR$uuid/wordpress/johnson_wotd.md"
+
+pandoc -s -o "$TMPDIR$uuid/wordpress/johnson_wotd.html" "$TMPDIR$uuid/wordpress/johnson_wotd.md"
+
+"$WP_BIN" post create "$TMPDIR$uuid/wordpress/johnson_wotd.html"  --post_type=post --post_status="publish" --post_title="Daily Dose of Samuel Johnson: $wotd_name & c." --post_mime_type=html --post_category="words-language" --post_author="$postauthorid"
