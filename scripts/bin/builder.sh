b4fefe6982ef49ca30ba7dfd0ee62e4726800ac3
Fred Zimmerman <wfzimmerman@gmail.com>
2018-08-21 22:02:42 -0400

    initial commits for wapackpedia enhancements

--- scripts/bin/builder.sh
+++ scripts/bin/builder.sh
@@ -8,10 +8,10 @@ mkdir /tmp/pagekicker/"$loguuid"
 
 touch /tmp/pagekicker/"$loguuid"/log
 
-# exec redirect below begins a rather convoluted process that is necessary to enable 
+# exec redirect below begins a rather convoluted process that is necessary to enable
 # the operation of a --verbose flag
 
-exec 3>&1 >> /tmp/pagekicker/"$loguuid"/startuplog 
+exec 3>&1 >> /tmp/pagekicker/"$loguuid"/startuplog
 
 
 echo "builder begins"
@@ -32,7 +32,7 @@ if shopt -q  login_shell ; then
 		echo "read config file from login shell $HOME""/.pagekicker/config.txt"
 	fi
 else
-	.  "$HOME"/.pagekicker/config.txt #hard-code here to have a nonlogin shell run the script 
+	.  "$HOME"/.pagekicker/config.txt #hard-code here to have a nonlogin shell run the script
 	echo "read config file from nonlogin shell /home/$(whoami)/.pagekicker/config.txt"
 fi
 
@@ -646,6 +646,10 @@ cp $confdir"jobprofiles"/imprints/"$imprint"/"$imprintlogo"  "$TMPDIR""$uuid"
 cp $confdir"jobprofiles"/signatures/"$sigfile" "$TMPDIR""$uuid"
 cp $confdir"jobprofiles"/imprints/"$imprint"/"$imprintlogo" "$TMPDIR"$uuid"/cover"
 
+# create placeholder seedfortheday file
+
+touch "$TMPDIR$uuid/wiki/seedfortheday.md"
+
 # extracts seeds from analyzed webpage
 
 if [ -z  ${analyze_url+x} ] ; then
@@ -679,76 +683,17 @@ echo "seeds are"
 cat "$TMPDIR$uuid/seeds/sorted.seedfile"
 echo "---"
 
-#expand seeds to valid wiki pages
 
-if [ "$expand_seeds_to_pages" = "yes" ] ; then
-		echo "$expand_seeds_to_pages"
-		"$PYTHON27_BIN" bin/wiki_seeds_2_pages.py --infile "$TMPDIR"$uuid"/seeds/sorted.seedfile" --pagehits "$TMPDIR"$uuid"/seeds/pagehits"
-else
-		echo "not expanding seeds to pages"
-		cp "$TMPDIR"$uuid"/seeds/sorted.seedfile" "$TMPDIR"$uuid"/seeds/pagehits"
-fi
+# Wikipedia search is on by default
 
-# filter pagehits
+. includes/wikipedia-default-loop.sh
 
-sort -u  "$TMPDIR"$uuid/seeds/pagehits >  "$TMPDIR"$uuid/seeds/filtered.pagehits
+# decides what additional search engines will be used
 
-echo "--- filtered pagehits are ---"
-cat  "$TMPDIR"$uuid/seeds/filtered.pagehits
-
-echo "--- end of pagehits ---"
-
-# fetch by pagehits
+. "$confdir""$jobprofilename"/search_engines/manifest.json
 
 
-case "$summary" in
-summaries_only)
-	echo "fetching page summaries only"
-	"$PYTHON_BIN"  $scriptpath"bin/wikifetcher.py" --infile "$TMPDIR$uuid /seeds/filtered.pagehits" --outfile "$TMPDIR"$uuid/"wiki/wikisummariesraw.md" --lang "$wikilocale" --summary  1> /dev/null
-	sed -e s/\=\=\=\=\=/JQJQJQJQJQ/g -e s/\=\=\=\=/JQJQJQJQ/g -e s/\=\=\=/JQJQJQ/g -e s/\=\=/JQJQ/g -e s/Edit\ /\ /g -e s/JQJQJQJQJQ/\#\#\#\#\#/g -e s/JQJQJQJQ/\#\#\#\#/g -e s/JQJQJQ/\#\#\#/g -e s/JQJQ/\#\#/g  "$TMPDIR"$uuid/wiki/wikisummariesraw.md | sed G >  "$TMPDIR"$uuid/wiki/wikisummaries.md
-	cp  "$TMPDIR"$uuid/wiki/wikisummaries.md  "$TMPDIR"$uuid/wiki/wikiall.md
-	wordcountsummaries=$(wc -w "$TMPDIR"$uuid/wiki/wikisummaries.md | cut -f1 -d' ')
-	cp "$TMPDIR"$uuid"/wiki/wikisummaries.md" "$TMPDIR"$uuid"/wiki/wiki4cloud.md"
-;;
-complete_pages_only)
-	echo "fetching complete pages only"
-	"$PYTHON_BIN" $scriptpath"bin/wikifetcher.py" --infile "$TMPDIR"$uuid"/seeds/filtered.pagehits" --outfile "$TMPDIR"$uuid"/wiki/wikipagesraw.md" --lang "$wikilocale"  1> /dev/null
-	sed -e s/\=\=\=\=\=/JQJQJQJQJQ/g -e s/\=\=\=\=/JQJQJQJQ/g -e s/\=\=\=/JQJQJQ/g -e s/\=\=/JQJQ/g -e s/Edit\ /\ /g -e s/JQJQJQJQJQ/\#\#\#\#\#/g -e s/JQJQJQJQ/\#\#\#\#/g -e s/JQJQJQ/\#\#\#/g -e s/JQJQ/\#\#/g  "$TMPDIR"$uuid/wiki/wikipagesraw.md | sed G >  "$TMPDIR"$uuid"/wiki/wikipages.md"
-	wordcountpages=$(wc -w "$TMPDIR"$uuid"/wiki/wikipages.md" | cut -f1 -d' ')
-	cp "$TMPDIR"$uuid"/wiki/wikipages.md" "$TMPDIR"$uuid"/wiki/wiki4cloud.md"
-	cp  "$TMPDIR"$uuid/wiki/wikipages.md  "$TMPDIR"$uuid/wiki/wikiall.md
-;;
-both)
-	echo "fetching both summaries and complete pages"
-	echo "fetching page summaries now"
-	"$PYTHON_BIN"  $scriptpath"bin/wikifetcher.py" --infile "$TMPDIR"$uuid"/seeds/filtered.pagehits" --outfile "$TMPDIR"$uuid"/wiki/wikisummaries1.md" --lang "$wikilocale" --summary
-	echo "fetching complete pages now"
-	"$PYTHON_BIN" $scriptpath"bin/wikifetcher.py" --infile "$TMPDIR"$uuid"/seeds/filtered.pagehits" --outfile "$TMPDIR"$uuid"/wiki/wikipages1.md" --lang "$wikilocale"
-	sed -e s/\=\=\=\=\=/JQJQJQJQJQ/g -e s/\=\=\=\=/JQJQJQJQ/g -e s/\=\=\=/JQJQJQ/g -e s/\=\=/JQJQ/g -e s/Edit\ /\ /g -e s/JQJQJQJQJQ/\#\#\#\#\#/g -e s/JQJQJQJQ/\#\#\#\#/g -e s/JQJQJQ/\#\#\#/g -e s/JQJQ/\#\#/g  "$TMPDIR"$uuid"/wiki/wikisummaries1.md" | sed G >  "$TMPDIR"$uuid/wiki/wikisummaries.md
-	sed -e s/\=\=\=\=\=/JQJQJQJQJQ/g -e s/\=\=\=\=/JQJQJQJQ/g -e s/\=\=\=/JQJQJQ/g -e s/\=\=/JQJQ/g -e s/Edit\ /\ /g -e s/JQJQJQJQJQ/\#\#\#\#\#/g -e s/JQJQJQJQ/\#\#\#\#/g -e s/JQJQJQ/\#\#\#/g -e s/JQJQ/\#\#/g  "$TMPDIR"$uuid"/wiki/wikipages1.md" | sed G >  "$TMPDIR"$uuid/wiki/wikipages.md
-
-wordcountpages=1
-	#wordcountpages=$(wc -w "$TMPDIR"$uuid"/wiki/wikipages.md" | cut -f1 -d' ')
-  wordcountpages=$(cat "$TMPDIR"$uuid"/wiki/wikipages.md" | tr '\n' ' ' | wc -w | tr -d ' ')
-	echo "Wordcount pages is" $wordcountpages
-		if [ "$wordcountpages" -gt 100000 ] ; then
-			cp  "$TMPDIR"$uuid/wiki/wikisummaries.md  "$TMPDIR"$uuid/wiki/wiki4cloud.md
-			cp  "$TMPDIR"$uuid/wiki/wikisummaries.md  "$TMPDIR"$uuid/wiki/wiki4chapters.md
-			echo "body too big for wordcloud, using abstracts only"
-		else
-			 cp "$TMPDIR$uuid/wiki/wikipages.md"  "$TMPDIR$uuid/wiki/wiki4cloud.md"
-			 cp  "$TMPDIR"$uuid/wiki/wikipages.md  "$TMPDIR"$uuid/wiki/wiki4chapters.md
-			echo "building wordcloud from body"
-		fi
-;;
-*)
-	echo "unrecognized summary option"
-;;
-esac
-
-# create placeholder seedfortheday file
-
-touch "$TMPDIR$uuid/wiki/seedfortheday.md"
+# adds user-provided content
 
 if [ "$add_this_content" = "none" ] ; then
 	echo "no added content"
