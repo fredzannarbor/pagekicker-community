3e4b84b0d388e74edfd7be34c3e53bb799a57cf4
Fred Zimmerman <wfzimmerman@gmail.com>
2018-11-02 09:49:10 -0400

    mwclient_wikifetcher can now be configured to point at any (one) Mediawiki server

--- scripts/bin/mwclient_wikifetcher.py
+++ scripts/bin/mwclient_wikifetcher.py
@@ -1,23 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-wikipedia text fetcher
+wapackpedia text fetcher
 Fred Zimmerman
 wfzimmerman#gmail.com
 
-converted to use mwclient libtary
-enhanced to allow pointing at any MediaWiki endpoint URL
+converted to use mwclient library and enhanced to allow pointing at any 
+MediaWiki endpoint URL
 
-fetches MW pages specified in --infile
-mwclient_seeds_to_pages.py is responsible for providing exact page names to infile
-page names must be exact, i.e. are case and punctuation sensitive
+three variables specify the endpoint URL
+--mediawiki_api_url "en.wikipedia.org" (no http)
+--url_prefix "http" or "https"
+--wikipath "/w/" is default -- path to API endpoint configured on Mediawiki server 
+
+mwclient_seeds_to_pages.py is responsible for providing 
+exact page names to infile page names must be exact, i.e. are case 
+and punctuation sensitive
 
 """
 
 import logging
 import argparse
 import mwclient
-from mwclient.errors import APIError, InvalidPageTitle
-import time
+
 
 logging.basicConfig(level=logging.WARNING)
 
@@ -28,9 +32,10 @@ parser.add_argument("--request_type", help="request type", default = 'sum')
 parser.add_argument("--outfile", help = "path to outfile", default = 'outfile')
 parser.add_argument("--summary", help = "true or false", action = "store_false")
 parser.add_argument("--logging", help = "true or false", action = "store_true")
-#parser.add_argument("--mediawiki_api_url", help = "true or false", default = 'en.wikipedia.org')
-#parser.add_argument("--wikipath", help = "mediawiki default is /w/api.php", default = '/w/')
-#parser.add_argument("--client_certificate", help = "path to SSL certificate", default = None)
+parser.add_argument("--mediawiki_api_url", help = "true or false", default = 'en.wikipedia.org')
+parser.add_argument("--url_prefix", help = "default wiki ssl value is https", default = 'https')
+parser.add_argument("--wikipath", help = "mediawiki default is /w/api.php", default = '/w/')
+
 args = parser.parse_args()
 
 input_file = args.infile
@@ -38,44 +43,24 @@ output_file = args.outfile
 lang = args.lang
 summary = args.summary
 logging = args.logging
-# mediawiki_api_url = args.mediawiki_api_url
-# wikipath = args.wikipath
-path = 'path=' 
-print(path)
-ep = "/w/"
-print(ep)
-#site = mwclient.Site(('http', 'test.wikipedia.org'), path)
-p2 = path  + "'" + ep + "'"
-print('p2 is ' + p2)
-site = mwclient.Site(('http', 'en.wikipedia.org'), path='/w/')
-#client_certificate = args.client_certificate
+mediawiki_api_url = args.mediawiki_api_url
+url_prefix = args.url_prefix
+wiki_tuple = (url_prefix, mediawiki_api_url)
+printline = "wiki_tuple value is{}".format(wiki_tuple)
+print(printline)
 
-"""
-providing client certificate option
-needs testing on ssl + cert environment
-
-if client_certificate is None:
-    foo = mwclient.Site(mediawiki_api_url)
-    print(foo)
-else:
-    print('client certificate is ' + client_certificate )
-    ssl_site1 = mediawiki_api_url + ', ' + 'client_certificate' + '='
-    print('ssl site 1 is' + ssl_site1)
-    ssl_site2 = ssl_site1 + client_certificate
-    print(ssl_site2)
-"""
-#site = mwclient.Site(mediawiki_api_url, 'path='wikipath)
+wikipath = args.wikipath
+site = mwclient.Site(wiki_tuple, path=wikipath)
 print(site)
 file = open(input_file, 'r').read().splitlines()
 file2 = open(output_file, 'wb')
 for line in file:
     try:
-        print(line)
+        print('seed is ' + line)
         page = site.pages[line]
         print(page)
         text = page.text()
         print(text)
-    # print(text)
     except:
         mwclient.errors.InvalidPageTitle
         continue
