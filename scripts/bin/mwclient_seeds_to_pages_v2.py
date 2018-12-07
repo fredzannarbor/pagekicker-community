574b21f8ed50bb47afde42ee2a92c07fab5c8c2e
Fred Zimmerman <wfzimmerman@gmail.com>
2018-12-07 16:27:10 -0500

    seed epansion working again for --seedfile parameter

--- scripts/bin/mwclient_seeds_to_pages_v2.py
+++ scripts/bin/mwclient_seeds_to_pages_v2.py
@@ -21,6 +21,7 @@ and punctuation sensitive
 import logging
 import argparse
 import mwclient
+from collections import OrderedDict
 
 
 logging.basicConfig(level=logging.WARNING)
@@ -36,12 +37,14 @@ parser.add_argument("--mediawiki_api_url", help = "true or false", default = 'en
 parser.add_argument("--url_prefix", help = "default wiki ssl value is https", default = 'https')
 parser.add_argument("--wikipath", help = "mediawiki default is /w/api.php", default = '/w/')
 parser.add_argument("--cats_file", help = "path to outfile", default = 'cats')
-parser.add_argument("--extlinks_file", help = "path to outfile", default = 'extlinks')
+parser.add_argument("--extlinks_file", help = "path to save extlinks file", default = 'extlinks')
 
 args = parser.parse_args()
 
 input_file = args.infile
 output_file = args.outfile
+print('output_file is ')
+print(output_file)
 cats_file = args.cats_file
 extlinks_file = args.extlinks_file
 lang = args.lang
@@ -53,56 +56,33 @@ wiki_tuple = (url_prefix, mediawiki_api_url)
 printline = "wiki_tuple value is{}".format(wiki_tuple)
 print(printline)
 
+def recursive_items(dictionary):
+    for key, value in dictionary.items():
+        if type(value) is OrderedDict:
+            #print('match')
+            yield from recursive_items(value)
+        else:
+            #print('no match')
+            yield (key, value)
+
 wikipath = args.wikipath
 site = mwclient.Site(wiki_tuple, path=wikipath)
 print(site)
 file1 = open(input_file, 'r').read().splitlines()
-file2 = open(output_file, 'w')
-file3 = open(cats_file, 'w')
-#file4 = open(extlinks_file, 'a+')
+
 for line in file1:
     try:
-        print('seed is ' + line)
+        #print('seed is ' + line)
         page = site.pages[line]
-        #print(page)
-        text = page.text()
-        #print(text)
-        #cats = page.categories('True', '!hidden')
-        #print('\n'+ 'categories are' + '\n')
-       #print(*cats, sep='\n')
-        cats = list(page.categories())
-        print(*cats)
-        extlinks = page.extlinks()
-        print('\n'+ 'external links are '+ '\n')
-        print(*extlinks, sep='\n', file=open(extlinks_file, "w"))
-        #backlinks = page.backlinks()
-        #print(*backlinks)
-        images = page.images()
-        print(*images)
+        d = site.api('query', generator='search', gsrsearch=line)
+        #print(d)
+        for key, value in recursive_items(d):
+            if 'title' in key:
+                print(value, file=open(output_file, 'a'))
+        #pagetitle_list = page.pagetitle()
+        #print(*pagetitle_list, sep='\n', file=open(output_file, "w"))
     except:
         mwclient.errors.InvalidPageTitle
         continue
-    file2.write('\n')
-    #print(text)
-    file2.write('\n')
-    file2.write('# ' )
-    file2.write(line)
-    file2.write('\n')
-    file2.write(text)
-    
-    file3.write('\n')
-    file3.write('\n')
-    file3.write('# ' )
-    file3.write(line)
-    file3.write('\n')
-    file3.write(str(cats))
-    
-#    file4.write('\n')
-#    file4.write('\n')
-#    file4.write('# ' )
-#    file4.write(line)
-#    file4.write('\n')
-# file4.write(str(extlinks))
-    
-file2.close
-file3.close
+
+
