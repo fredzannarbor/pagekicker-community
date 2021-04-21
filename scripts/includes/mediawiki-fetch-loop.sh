f6b681a50726e0f1ecea866c918881cb1c6276a5
Fred Zimmerman <fredzannarbor@gmail.com>
2021-04-20 21:52:31 -0400

    successfully received all wiki data & metadata

--- scripts/includes/mediawiki-fetch-loop.sh
+++ scripts/includes/mediawiki-fetch-loop.sh
@@ -22,8 +22,8 @@ echo "wikipath is $wikipath"
 case "$summary" in
 summaries_only)
 	echo "fetching page summaries only"
-	"$PYTHON_BIN"  $scriptpath"bin/mwclient_wikifetcher.py" --infile "$TMPDIR$uuid/seeds/filtered.pagehits" --outfile "$TMPDIR"$uuid/"wiki/wikisummariesraw.md" --lang "$wikilocale" --wikipath "$wikipath" --url_prefix "$url_prefix" --mediawiki_api_url "$mediawiki_api_url" --summary  1> /dev/null
-	#sed -e s/\=\=\=\=\=/JQJQJQJQJQ/g -e s/\=\=\=\=/JQJQJQJQ/g -e s/\=\=\=/JQJQJQ/g -e s/\=\=/JQJQ/g -e s/Edit\ /\ /g -e s/JQJQJQJQJQ/\#\#\#\#\#/g -e s/JQJQJQJQ/\#\#\#\#/g -e s/JQJQJQ/\#\#\#/g -e s/JQJQ/\#\#/g  "$TMPDIR"$uuid/wiki/wikisummariesraw.md | sed G >  "$TMPDIR"$uuid/wiki/wikisummaries.md
+	"$PYTHON_BIN"  $scriptpath"bin/mwclient_wikifetcher.py" --infile "$TMPDIR$uuid/seeds/filtered.pagehits" --outfile "wikisummariesraw.md" --lang "$wikilocale" --wikipath "$wikipath" --url_prefix "$url_prefix" --mediawiki_api_url "$mediawiki_api_url" --summary  --outdir "$TMPDIR"$uuid"/wiki"  1> /dev/null
+
   pandoc -o "$TMPDIR"$uuid/wiki/wikisummaries.md --f mediawiki -t markdown "$TMPDIR"$uuid/wiki/wikisummariesraw.md
 	cp  "$TMPDIR"$uuid/wiki/wikisummaries.md  "$TMPDIR"$uuid/wiki/wikiall.md
 	wordcountsummaries=$(wc -w "$TMPDIR"$uuid/wiki/wikisummaries.md | cut -f1 -d' ')
@@ -31,8 +31,8 @@ summaries_only)
 ;;
 complete_pages_only)
 	echo "fetching complete pages only"
-	"$PYTHON_BIN" $scriptpath"bin/mwclient_wikifetcher.py" --infile "$TMPDIR"$uuid"/seeds/filtered.pagehits" --outfile "$TMPDIR"$uuid"/wiki/wikipagesraw.md" --lang "$wikilocale"  --wikipath "$wikipath" --url_prefix "$url_prefix" --mediawiki_api_url "$mediawiki_api_url"  1> /dev/null
-	#sed -e s/\=\=\=\=\=/JQJQJQJQJQ/g -e s/\=\=\=\=/JQJQJQJQ/g -e s/\=\=\=/JQJQJQ/g -e s/\=\=/JQJQ/g -e s/Edit\ /\ /g -e s/JQJQJQJQJQ/\#\#\#\#\#/g -e s/JQJQJQJQ/\#\#\#\#/g -e s/JQJQJQ/\#\#\#/g -e s/JQJQ/\#\#/g  "$TMPDIR"$uuid/wiki/wikipagesraw.md | sed G >  "$TMPDIR"$uuid"/wiki/wikipages.md"
+	"$PYTHON_BIN" $scriptpath"bin/mwclient_wikifetcher.py" --infile "$TMPDIR"$uuid"/seeds/filtered.pagehits" --outfile "wikipagesraw.md" --lang "$wikilocale"  --wikipath "$wikipath" --url_prefix "$url_prefix" --mediawiki_api_url "$mediawiki_api_url" --outdir "$TMPDIR"$uuid"/wiki" 1> /dev/null
+
   pandoc -o "$TMPDIR"$uuid"/wiki/wikipages.md" -f mediawiki -t markdown "$TMPDIR"$uuid/wiki/wikipagesraw.md
 	wordcountpages=$(wc -w "$TMPDIR"$uuid"/wiki/wikipages.md" | cut -f1 -d' ')
 	cp "$TMPDIR"$uuid"/wiki/wikipages.md" "$TMPDIR"$uuid"/wiki/wiki4cloud.md"
@@ -41,17 +41,17 @@ complete_pages_only)
 both)
 	echo "fetching both summaries and complete pages"
 	echo "fetching page summaries now"
-	"$PYTHON_BIN"  $scriptpath"bin/mwclient_wikifetcher.py" --infile "$TMPDIR"$uuid"/seeds/filtered.pagehits" --outfile "$TMPDIR"$uuid"/wiki/wikisummaries1.mw" --lang "$wikilocale"  --wikipath "$wikipath" --url_prefix "$url_prefix" --mediawiki_api_url "$mediawiki_api_url" --summary
+	"$PYTHON_BIN"  $scriptpath"bin/mwclient_wikifetcher.py" --infile "$TMPDIR"$uuid"/seeds/filtered.pagehits" --outfile "wikisummaries1.mw" --lang "$wikilocale"  --wikipath "$wikipath" --url_prefix "$url_prefix" --mediawiki_api_url "$mediawiki_api_url" --summary --outdir "$TMPDIR"$uuid"/wiki"
 	echo "fetching complete pages now"
-	"$PYTHON_BIN" $scriptpath"bin/mwclient_wikifetcher.py" --infile "$TMPDIR"$uuid"/seeds/filtered.pagehits" --outfile "$TMPDIR"$uuid"/wiki/wikipages1.mw" --lang "$wikilocale"  --wikipath "$wikipath" --url_prefix "$url_prefix" --mediawiki_api_url "$mediawiki_api_url"
-	#sed -e s/\=\=\=\=\=/JQJQJQJQJQ/g -e s/\=\=\=\=/JQJQJQJQ/g -e s/\=\=\=/JQJQJQ/g -e s/\=\=/JQJQ/g -e s/Edit\ /\ /g -e s/JQJQJQJQJQ/\#\#\#\#\#/g -e s/JQJQJQJQ/\#\#\#\#/g -e s/JQJQJQ/\#\#\#/g -e s/JQJQ/\#\#/g  "$TMPDIR"$uuid"/wiki/wikisummaries1.md" | sed G >  "$TMPDIR"$uuid/wiki/wikisummaries.md
+	"$PYTHON_BIN" $scriptpath"bin/mwclient_wikifetcher.py" --infile "$TMPDIR"$uuid"/seeds/filtered.pagehits" --outfile "wikipages1.mw" --lang "$wikilocale"  --wikipath "$wikipath" --url_prefix "$url_prefix" --mediawiki_api_url "$mediawiki_api_url" --outdir "$TMPDIR"$uuid"/wiki"
+
   pandoc -o "$TMPDIR"$uuid/wiki/wikisummaries.md -f mediawiki -t markdown "$TMPDIR"$uuid/wiki/wikisummaries1.mw
-	#sed -e s/\=\=\=\=\=/JQJQJQJQJQ/g -e s/\=\=\=\=/JQJQJQJQ/g -e s/\=\=\=/JQJQJQ/g -e s/\=\=/JQJQ/g -e s/Edit\ /\ /g -e s/JQJQJQJQJQ/\#\#\#\#\#/g -e s/JQJQJQJQ/\#\#\#\#/g -e s/JQJQJQ/\#\#\#/g -e s/JQJQ/\#\#/g  "$TMPDIR"$uuid"/wiki/wikipages1.md" | sed G >  "$TMPDIR"$uuid/wiki/wikipages.md
+
 
   pandoc -o "$TMPDIR"$uuid"/wiki/wikipages.md"  -t markdown -f mediawiki "$TMPDIR"$uuid"/wiki/wikipages1.mw"
 
 	wordcountpages=1
-	#wordcountpages=$(wc -w "$TMPDIR"$uuid"/wiki/wikipages.md" | cut -f1 -d' ')
+
   wordcountpages=$(cat "$TMPDIR"$uuid"/wiki/wikipages.md" | tr '\n' ' ' | wc -w | tr -d ' ')
 	echo "Wordcount pages is" $wordcountpages
 		if [ "$wordcountpages" -gt 100000 ] ; then
